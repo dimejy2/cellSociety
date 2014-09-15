2f2fa8cc4203228f016d7b506f06a7e2696d87d6
Ethan Chang <ejc24@duke.edu>
2014-09-14 21:09:05 -0400

    Began using gridview

--- src/cellsociety_team05_controller/CellAutomata.java
+++ src/cellsociety_team05_controller/CellAutomata.java
@@ -4,13 +4,21 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class CellAutomata extends Application {
 	SimulationRules mySimulation;
 	
-	private Board createBoard(String xml) {
-			
+	private void setUpGrid(GridPane grid, int width, int height) {
+		for(int i=0; i < width; i++) {
+			grid.getColumnConstraints().add(new ColumnConstraints(400/width));
+		}
+		for(int i=0; i< height; i++) {
+			grid.getRowConstraints().add(new RowConstraints(400/height));
+		}
 	}
 	
 	@Override
@@ -18,8 +26,11 @@ public class CellAutomata extends Application {
 		// TODO Auto-generated method stub
 		s.setTitle("Cellular Automata");
 		mySimulation = new SimulationRules(); // Your simulation's class
-		Board board = createBoard(xml);
-		Scene scene = mySimulation.init(s, 400, 400, board);
+//		Board board = createBoard(xml); For when we get XML working
+		GridPane grid = new GridPane();
+		setUpGrid(grid, 8, 8);
+		Board board = new Board(grid);
+		Scene scene = mySimulation.init(s, 400, 400, board, grid);
 		s.setScene(scene);
 		s.show();
 		
--- src/cellsociety_team05_controller/SimulationRules.java
+++ src/cellsociety_team05_controller/SimulationRules.java
@@ -10,25 +10,25 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public abstract class SimulationRules {
+abstract class SimulationRules {
 	
 	private Board myBoard;
-	private ArrayList<Cell> nextBoard;
+	private ArrayList<Cell> nextBoardCells;
 	private CellController myCellController;
 	
-	public Scene init (Stage s, int width, int height, Board board) {
-		// Create a scene graph to organize the scene
-		Group root = new Group();
+	public Scene init (Stage s, int width, int height, GridPane grid) {
+
 		// Create a place to see the shapes
-		Scene scene = new Scene(root, width, height, Color.WHITE);
-		myCellController = new CellController();
+		Scene scene = new Scene(grid, width, height, Color.WHITE);
+		myCellController = new CellController(); // Your simulation's CellController
 		myBoard = board;
-		root.getChildren().add(myBoard);
+
 		return scene;
 	}
 	
