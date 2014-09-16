5a9788fd21841f7c9991083de7462d9a2d0c9ef5
Sandy Lee <sl269@duke.edu>
2014-09-16 19:54:46 -0400

    moved some methods that can be used in all simulations to the superclass, SimulationRules.

--- src/cellsociety_team05_controller/DummySimulationRules.java
+++ src/cellsociety_team05_controller/DummySimulationRules.java
@@ -11,11 +11,11 @@ public class DummySimulationRules extends SimulationRules {
 	@Override
 	protected int checkCells(Cell cell, Cell neighbour) {
 		// TODO Auto-generated method stub
-		int alikeNeighbours = 0;
+		int liveNeighbours = 0;
 		if (neighbour != null && (cell.getState() == neighbour.getState())) {
-			alikeNeighbours++;
+			liveNeighbours++;
 		}
-		return alikeNeighbours;
+		return liveNeighbours;
 	}
 
 	@Override
@@ -25,36 +25,6 @@ public class DummySimulationRules extends SimulationRules {
 		}
 		return 1; // your original state (index #)
 	}
-
-	// something like this maybe...:(
-	public Color stateToColor(int state) {
-		if (state == 0) {
-			return Color.RED;
-		}
-		return Color.GREEN;
-	}
-
-	// deal with corner cases
-	@Override
-	protected void updateBoard(Board board) {
-		// TODO Auto-generated method stub
-		myCells = board.getCells();
-		nextBoardCells = new Cell[board.getRow()][board.getColumn()];
-		for (int i = 0; i < board.getRow(); i++) {
-			for (int j = 0; j < board.getColumn(); j++) {
-				Cell cell = myCells[i][j];
-				for (int[] n : neighbourMap) {
-					Cell neighbour = myCells[cell.getXPosition() + n[0]][cell
-							.getYPosition() + n[1]];
-					int alikeNeighbours = checkCells(cell, neighbour);
-					int nextState = nextState(alikeNeighbours);
-					// myCellController.updateCell(cell, nextState, color)
-					// associate color with state maybe? below to another method
-					// maybe
-					Color color = stateToColor(nextState);
-					myCellController.updateCell(cell, nextState, color);
-				}
-			}
-		}
-	}
+		
+	
 }
--- src/cellsociety_team05_controller/SimulationRules.java
+++ src/cellsociety_team05_controller/SimulationRules.java
@@ -14,38 +14,66 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 abstract class SimulationRules {
-	
+
 	private Board myBoard;
-	protected Cell[][] myCells; //i added
+	protected Cell[][] myCells;
 	protected Cell[][] nextBoardCells;
 	protected CellController myCellController;
 	protected static final int[][] neighbourMap = { { -1, -1 }, { 0, -1 },
-		{ 1, -1 }, { -1, 0 }, { +1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
+			{ 1, -1 }, { -1, 0 }, { +1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 } };
 
-	
-	public Scene init (Stage s, int width, int height, GridPane grid) {
+	public Scene init(Stage s, int width, int height, GridPane grid) {
 
 		// Create a place to see the shapes
 		Scene scene = new Scene(grid, width, height, Color.WHITE);
-		myCellController = new DummyCellController(); // Your simulation's CellController
+		myCellController = new DummyCellController(); // Your simulation's
+														// CellController
 		return scene;
 	}
-	
-    private EventHandler<ActionEvent> oneFrame = new EventHandler<ActionEvent>() {
+
+	private EventHandler<ActionEvent> oneFrame = new EventHandler<ActionEvent>() {
 		@Override
 		public void handle(ActionEvent evt) {
-//			checkCells(myBoard);
+			// checkCells(myBoard);
 			updateBoard(myBoard);
 		}
 	};
-	
+
 	protected abstract int checkCells(Cell cell, Cell neighbour);
-	
+
 	public abstract int nextState(int alikeNeighbours);
-	
-	protected abstract void updateBoard(Board board);
-	
-	public KeyFrame start () {
-		return new KeyFrame(Duration.millis(1000/60), oneFrame);
+
+	public void updateBoard(Board board) {
+		// TODO Auto-generated method stub
+		myCells = board.getCells();
+		nextBoardCells = new Cell[board.getRow()][board.getColumn()];
+		for (int i = 0; i < board.getRow(); i++) {
+			for (int j = 0; j < board.getColumn(); j++) {
+				Cell cell = myCells[i][j];
+				for (int[] n : neighbourMap) {
+					Cell neighbour = myCells[cell.getXPosition() + n[0]][cell
+							.getYPosition() + n[1]];
+					int alikeNeighbours = checkCells(cell, neighbour);
+					int nextState = nextState(alikeNeighbours);
+					// myCellController.updateCell(cell, nextState, color)
+					// associate color with state maybe? below to another method
+					// maybe
+					Color color = stateToColor(nextState);
+					myCellController.updateCell(cell, nextState, color);
+				}
+			}
+		}
+	}
+
+	//just an example. color for each state should be dealt with in cellview?
+	public Color stateToColor(int state) {
+		if (state == 0) {
+			return Color.RED;
+		}
+		return Color.GREEN;
+	}
+
+	public KeyFrame start() {
+		return new KeyFrame(Duration.millis(1000 / 60), oneFrame);
 	}
 }
