d9654b09b9fdd26159257af08f5b273ccf21942b
Ethan Chang <ejc24@duke.edu>
2014-09-19 03:46:43 -0400

    completed watorworld

--- src/cellsociety_team05_controller/PredatorPreySimulation.java
+++ src/cellsociety_team05_controller/PredatorPreySimulation.java
@@ -18,7 +18,6 @@ public class PredatorPreySimulation extends SimulationRules {
 		int fish = 1;
 		int shark = 2;
 		cell.incrementFramesAlive(); //updates alive
-
 		if(invalidCellChoices.contains(cell)) {
 
 		}
@@ -27,13 +26,24 @@ public class PredatorPreySimulation extends SimulationRules {
 			if(emptyNeighbors.size() > 0) {
 				int randomNum = rand.nextInt(emptyNeighbors.size()); // generate a random integer bounded by the number of empty neighbors
 				while(emptyNeighbors.size() >0 && invalidCellChoices.contains(emptyNeighbors.get(randomNum))) {
-					randomNum = rand.nextInt(emptyNeighbors.size());
 					emptyNeighbors.remove(randomNum);
-
+					if(emptyNeighbors.size() ==0) {
+						break;
+					}
+					randomNum = rand.nextInt(emptyNeighbors.size());
 				}
 				if(emptyNeighbors.size() > 0) {
-					Cell newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
-					newCell.getCellView().setColor(stateToColorMap.get(0));
+					Cell newCell;
+					if(cell.getFramesAlive()==5) {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), cell.getState(), cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(cell.getState()));
+						cell.resetFramesAlive();
+					}
+					else {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(0));
+
+					}
 					nextBoardCells[cell.getRow()][cell.getColumn()] = newCell;
 					nextBoardCells[emptyNeighbors.get(randomNum).getRow()][emptyNeighbors.get(randomNum).getColumn()] = cell;
 					invalidCellChoices.add(emptyNeighbors.get(randomNum));
@@ -44,12 +54,10 @@ public class PredatorPreySimulation extends SimulationRules {
 				}
 				else {
 					nextBoardCells[cell.getRow()][cell.getColumn()] = cell;	
-					invalidCellChoices.add(cell);
 				}
 			}
 			else {
 				nextBoardCells[cell.getRow()][cell.getColumn()] = cell;
-				invalidCellChoices.add(cell);
 			}
 		}
 
@@ -59,37 +67,81 @@ public class PredatorPreySimulation extends SimulationRules {
 				int randomNum = rand.nextInt(fishNeighbors.size());
 				Cell fishCell = fishNeighbors.get(randomNum);
 				while(fishNeighbors.size() > 0 && invalidCellChoices.contains(fishCell)) {
-					randomNum = rand.nextInt(fishNeighbors.size());
-					fishCell = fishNeighbors.get(randomNum);
 					fishNeighbors.remove(randomNum);
+					if(fishNeighbors.size() <=0) {
+						break;
+					}
+					randomNum = rand.nextInt(fishNeighbors.size());
 
 				}
 				if(fishNeighbors.size()>0) {
+					Cell newCell;
+					if(cell.getFramesAlive()==5) {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), cell.getState(), cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(cell.getState()));
+						newCell.setResources(6);
+						cell.resetFramesAlive();
+					}
+					else {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(0));
+					}					
 
-					Cell newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
-					newCell.getCellView().setColor(stateToColorMap.get(0));
-					nextBoardCells[newCell.getRow()][newCell.getColumn()] = newCell;
+					nextBoardCells[cell.getRow()][cell.getColumn()] = newCell;
 					nextBoardCells[fishCell.getRow()][fishCell.getColumn()] = cell;
 					cell.setRow(fishCell.getRow());
 					cell.setColumn(fishCell.getColumn());
+					cell.incrementResources(2);
 					invalidCellChoices.add(fishCell);
 					invalidCellChoices.add(newCell);
 					invalidCellChoices.add(cell);
 				}
 			}
+			if(fishNeighbors.size()<1) {
+				cell.incrementResources(-2);
+				if(cell.getResources() ==0) {
+					cell.setState(0);
+					cell.getCellView().setColor(stateToColorMap.get(cell.getState()));
+					invalidCellChoices.add(cell);
+					nextBoardCells[cell.getRow()][cell.getColumn()] = cell;
+				}
+			}
+
 			if(cell.getNeighborMap().get(empty).size() > 0 && fishNeighbors.size()<1) {
 				ArrayList<Cell> emptyNeighbors = cell.getNeighborMap().get(empty);
 				int randomNum = rand.nextInt(emptyNeighbors.size());
-				Cell newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
-				Cell emptyCell = emptyNeighbors.get(randomNum);
-				newCell.getCellView().setColor(stateToColorMap.get(0));
-				nextBoardCells[cell.getRow()][cell.getColumn()] = newCell;
-				nextBoardCells[emptyCell.getRow()][emptyCell.getColumn()] = cell;
-				invalidCellChoices.add(emptyCell);
-				invalidCellChoices.add(cell);
-				invalidCellChoices.add(newCell);
-				cell.setRow(emptyNeighbors.get(randomNum).getRow());
-				cell.setColumn(emptyNeighbors.get(randomNum).getColumn());
+				while(emptyNeighbors.size() >0 && invalidCellChoices.contains(emptyNeighbors.get(randomNum))) {
+					emptyNeighbors.remove(randomNum);
+					if(emptyNeighbors.size() ==0) {
+						break;
+					}
+					randomNum = rand.nextInt(emptyNeighbors.size());
+				}
+				if(emptyNeighbors.size()>0) {
+					Cell newCell;
+					if(cell.getFramesAlive()==5) {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), cell.getState(), cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(cell.getState()));
+						newCell.setResources(6);
+						cell.resetFramesAlive();
+					}
+					else {
+						newCell =  new Cell(cell.getRow(), cell.getColumn(), 0, cell.getCellDim());
+						newCell.getCellView().setColor(stateToColorMap.get(0));
+					}	
+					Cell emptyCell = emptyNeighbors.get(randomNum);
+					nextBoardCells[cell.getRow()][cell.getColumn()] = newCell;
+					nextBoardCells[emptyCell.getRow()][emptyCell.getColumn()] = cell;
+					invalidCellChoices.add(emptyCell);
+					invalidCellChoices.add(cell);
+					invalidCellChoices.add(newCell);
+					cell.setRow(emptyNeighbors.get(randomNum).getRow());
+					cell.setColumn(emptyNeighbors.get(randomNum).getColumn());
+				}
+				else {
+					nextBoardCells[cell.getRow()][cell.getColumn()] = cell;	
+					invalidCellChoices.add(cell);
+				}
 			}
 			else {
 				nextBoardCells[cell.getRow()][cell.getColumn()] = cell;	
